b65936f8b825ca330bd54fffdef53e6f308f2e60
theluk <theluk@gmail.com>
2016-04-30 14:44:33 +0200

    module

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,38 @@
-var gulp = require('gulp');
-var request = require('request');
-var del = require('del');
-var fs = require('fs');
+const gulp = require('gulp');
+const request = require('request');
+const del = require('del');
+const fs = require('fs');
+const template = require('gulp-template');
+const rename = require('gulp-rename');
 
 const JSFILE = 'https://raw.githubusercontent.com/Microsoft/ProjectOxford-ClientSDK/master/Speech/Speech.JS/speech.1.0.0.js';
 
 gulp.task('clean', function() {
   // You can use multiple globbing patterns as you would with `gulp.src`
-  return del(['speech.js']);
+  return del(['_speech.js', 'speech.js']);
 });
 
 gulp.task('copy-original', ['clean'], function () {
   return request({url:JSFILE})
     .pipe(require('stream').PassThrough())
-    .pipe(fs.createWriteStream('speech.js'));
-}); 
+    .pipe(fs.createWriteStream('_speech.js'));
+});
+
+gulp.task('build', function() {
+    try {
+        var content = fs.readFileSync('_speech.js').toString();    
+    } catch (error) {
+        console.error("ERROR: _speech.js does not exist, try running `gulp copy-original` first");
+        return false;
+    }
+    // fix. Microsoft calls initialization when loading the module. 
+    // this throws errors when in node 
+    content = content.replace('SpeechMain();', 'Bing.SpeechMain = SpeechMain;');
+    
+    gulp.src(['template.js.tmpl'])
+        .pipe(template({
+            content : content
+        }))
+        .pipe(rename('speech.js'))
+        .pipe(gulp.dest('./'));
+})
--- speech.js
+++ speech.js
@@ -1,4 +1,22 @@
-//----------------------------------------------------------------------
+// if the module has no dependencies, the above pattern can be simplified to
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define([], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory(root);
+    } else {
+        // Browser globals (root is window)
+        var ns = factory(root);
+        root.Bing = ns.Bing;
+        root.Microsoft = ns.Microsoft;
+  }
+}(this, function (window) {
+
+    //----------------------------------------------------------------------
 // Microsoft Speech SDK
 // ====================
 // 
@@ -13,8 +31,8 @@
 // 
 // LICENSE
 // -------
-// © 2015 Microsoft. All rights reserved.  
-// This document is provided “as-is”. Information and views expressed in this document, including URL and other Internet Web site references, may change without notice.  
+// ï¿½ 2015 Microsoft. All rights reserved.  
+// This document is provided ï¿½as-isï¿½. Information and views expressed in this document, including URL and other Internet Web site references, may change without notice.  
 // Some examples depicted herein are provided for illustration only and are fictitious.  No real association or connection is intended or should be inferred. 
 // This document does not provide you with any legal rights to any intellectual property in any Microsoft product. You may copy and use this document for your internal, reference purposes. This 
 // document is confidential and proprietary to Microsoft. It is disclosed and can be used only pursuant to a non-disclosure agreement. 
@@ -1493,5 +1511,13 @@ var Bing;
         Bing._window.speechSynthesis = Bing._window.msSpeechSynthesis;
         Bing.initialize();
     }
-    SpeechMain();
+    Bing.SpeechMain = SpeechMain;
 })(Bing || (Bing = {}));
+
+    
+    return {
+        Bing : Bing,
+        Microsoft : Microsoft
+    };
+    
+}));
